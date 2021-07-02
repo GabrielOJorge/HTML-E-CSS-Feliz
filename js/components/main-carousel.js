17436c10df596e815df6aaaed45f87c8d8c9c801
Gabriel O. Jorge <gabrieloliveiraj331@gmail.com>
2021-07-01 23:44:48 -0300

    Create the about section

---
+++ js/components/main-carousel.js
@@ -0,0 +1,16 @@
+const btnRight = document.querySelector('.btn-arrow.-right')
+const btnLeft = document.querySelector('.btn-arrow.-left')
+const elements = document.querySelector('.elements')
+let pixels = 100;
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels + 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+console.log(elements)
